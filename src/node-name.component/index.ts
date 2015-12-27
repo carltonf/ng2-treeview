2f5e3f20de66acfea2c578f0166cd1b025789467
Carl Xiong <xiongc05@gmail.com>
2015-12-27 16:25:10 +0800

    feat(dnd): setup <node-name> as drag drop target

--- src/node-name.component/index.ts
+++ src/node-name.component/index.ts
@@ -2,15 +2,16 @@ import { Component } from 'angular2/core';
 import { NgSwitch, NgSwitchDefault, NgSwitchWhen } from 'angular2/common';
 
 @Component({
-    selector: 'node-name',
-    moduleId: module.id,
-    templateUrl: 'tpl.html',
-    styleUrls: ['style.css'],
-    inputs: ['name'],
+  selector: 'node-name',
+  moduleId: module.id,
+  templateUrl: 'tpl.html',
+  styleUrls: ['style.css'],
+  inputs: ['name'],
 })
 export class NodeNameComponent {
   public name: string;
   isDragging: boolean = false;
+  isDraggingOver: boolean = false;
 
   ondragstart(event: DragEvent, nameBox) {
     // todo: take advantage of dataTransfer interface
@@ -21,5 +22,26 @@ export class NodeNameComponent {
 
   ondragend() {
     this.isDragging = false;
+  }
+
+  ondragover(event) {
+    // prevent default to allow drop
+    event.preventDefault();
+  }
+  ondragenter() {
+    this.isDraggingOver = true;
+  }
+
+  ondragleave() {
+    this.isDraggingOver = false;
+  }
+
+  ondrop(event, box) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+
+    this.isDraggingOver = false;
+    // todo really do something
+    console.log('Drop a child at ' + box);
   }
 }
