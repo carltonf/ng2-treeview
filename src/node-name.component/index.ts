43f818e0946080b809d39dd366bcdc8dcd821658
Carl Xiong <xiongc05@gmail.com>
2015-12-27 15:57:40 +0800

    feat(dnd): basic styling for drag begin&end

--- src/node-name.component/index.ts
+++ src/node-name.component/index.ts
@@ -10,4 +10,16 @@ import { NgSwitch, NgSwitchDefault, NgSwitchWhen } from 'angular2/common';
 })
 export class NodeNameComponent {
   public name: string;
+  isDragging: boolean = false;
+
+  ondragstart(event: DragEvent, nameBox) {
+    // todo: take advantage of dataTransfer interface
+    event.dataTransfer.setData('text/plain', null);
+
+    this.isDragging = true;
+  }
+
+  ondragend() {
+    this.isDragging = false;
+  }
 }
